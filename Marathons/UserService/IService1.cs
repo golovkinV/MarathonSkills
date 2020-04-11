c2195f975bc99d2637953114f0d264f0b0dc0fdb
golovkinV <vladimir.golovkin16@yandex.ru>
2020-04-11 23:06:31 +0500

    fixes: delete old fields

--- Marathons/UserService/IService1.cs
+++ Marathons/UserService/IService1.cs
@@ -7,7 +7,7 @@ using System.Text;
 
 namespace UserService
 {
-    // ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Рефакторинг" можно использовать для одновременного изменения имени интерфейса "IService1" в коде и файле конфигурации.
+    
     [ServiceContract]
     public interface IService1
     {
--- Marathons/UserService/UserRequest.cs
+++ Marathons/UserService/UserRequest.cs
@@ -6,7 +6,29 @@ using System.Threading.Tasks;
 
 namespace UserService
 {
-    class UserRequest
+    public class UserRequest
     {
+        public static string User(string email, string password)
+        {
+            return $"SELECT * FROM [User] WHERE Email = '{email}' AND Password = '{password}'";
+        }
+
+        public static string RunnerData(string email)
+        {
+            return $"SELECT Runner.Gender, CONVERT(varchar, Runner.DateOfBirth, 104) as Date, Country.CountryCode, Country.CountryName " +
+                $"FROM([Runner] INNER JOIN [Country] ON Runner.CountryCode = Country.CountryCode) WHERE Email = '{email}'";
+        }
+
+        public static string UpdateUser(User user)
+        {
+            return $"UPDATE [User] SET Password = '{user.password}', FirstName = '{user.firstName}', LastName = '{user.lastName}' " +
+                $"WHERE Email = '{user.email}'";
+        }
+
+        public static string UpdateRunnerData(RunnerData runner, string email)
+        {
+            return $"UPDATE [Runner] SET Gender = '{runner.gender}', DateOfBirth = '{Convert.ToDateTime(runner.birthday)}', CountryCode = '{runner.countryCode}' " +
+                $"WHERE Email = '{email}'";
+        }
     }
 }
