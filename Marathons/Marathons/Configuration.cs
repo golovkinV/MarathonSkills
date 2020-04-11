3491b68c3ff510db5f7e37c084b460e2b115a5a7
golovkinV <vladimir.golovkin16@yandex.ru>
2020-04-11 22:39:40 +0500

    add: support classes

---
+++ Marathons/Marathons/Configuration.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Marathons
+{
+    public struct Configuration
+    {
+        public static string localServer = "Data Source=LAPTOP-DH0P6EPS;Initial Catalog=dbMarathon;Integrated Security=True";
+
+        public static string someeServer = "workstation id=dbMarathon.mssql.somee.com;packet size=4096;user id=que01_SQLLogin_1;pwd=jka6ldyw28;data source=dbMarathon.mssql.somee.com;persist security info=False;initial catalog=dbMarathon";
+    }
+}
--- Marathons/UserService/UserRequest.cs
+++ Marathons/UserService/UserRequest.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Marathons
+namespace UserService
 {
-    public class Class1
+    class UserRequest
     {
     }
 }
