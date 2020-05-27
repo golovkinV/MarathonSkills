5d7a4385320020a0c26e7f6b4fe247de4bdc64bc
Айдар Сайфутдинов <stud0000192492@study.utmn.ru>
2020-05-27 15:49:51 +0500

    "Find out more information" form

---
+++ Marathons/Client/FindOutMore/FindOutMoreForm.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace Client.FindOutMore
+{
+    partial class FindOutMoreForm:BaseForm.BaseForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.marathonSkills2015Button = new System.Windows.Forms.Button();
+            this.previosRaceResultsButton = new System.Windows.Forms.Button();
+            this.bmiCalculatorButton = new System.Windows.Forms.Button();
+            this.bmrCalculatorButton = new System.Windows.Forms.Button();
+            this.listOfCharitiesButton = new System.Windows.Forms.Button();
+            this.howLongIsAMarathonButton = new System.Windows.Forms.Button();
+            this.findOutMoreInformationLabel = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.footerPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Size = new System.Drawing.Size(800, 65);
+            // 
+            // footerPanel
+            // 
+            this.footerPanel.Location = new System.Drawing.Point(0, 414);
+            this.footerPanel.Size = new System.Drawing.Size(800, 36);
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            // 
+            // marathonSkills2015Button
+            // 
+            this.marathonSkills2015Button.Location = new System.Drawing.Point(109, 142);
+            this.marathonSkills2015Button.Name = "marathonSkills2015Button";
+            this.marathonSkills2015Button.Size = new System.Drawing.Size(183, 73);
+            this.marathonSkills2015Button.TabIndex = 20;
+            this.marathonSkills2015Button.Text = "Marathons Skills 2015";
+            this.marathonSkills2015Button.UseVisualStyleBackColor = true;
+            // 
+            // previosRaceResultsButton
+            // 
+            this.previosRaceResultsButton.Location = new System.Drawing.Point(109, 230);
+            this.previosRaceResultsButton.Name = "previosRaceResultsButton";
+            this.previosRaceResultsButton.Size = new System.Drawing.Size(183, 73);
+            this.previosRaceResultsButton.TabIndex = 21;
+            this.previosRaceResultsButton.Text = "Previos Race Results";
+            this.previosRaceResultsButton.UseVisualStyleBackColor = true;
+            // 
+            // bmiCalculatorButton
+            // 
+            this.bmiCalculatorButton.Location = new System.Drawing.Point(109, 326);
+            this.bmiCalculatorButton.Name = "bmiCalculatorButton";
+            this.bmiCalculatorButton.Size = new System.Drawing.Size(183, 73);
+            this.bmiCalculatorButton.TabIndex = 22;
+            this.bmiCalculatorButton.Text = "BMI calculator";
+            this.bmiCalculatorButton.UseVisualStyleBackColor = true;
+            // 
+            // bmrCalculatorButton
+            // 
+            this.bmrCalculatorButton.Location = new System.Drawing.Point(411, 326);
+            this.bmrCalculatorButton.Name = "bmrCalculatorButton";
+            this.bmrCalculatorButton.Size = new System.Drawing.Size(183, 73);
+            this.bmrCalculatorButton.TabIndex = 25;
+            this.bmrCalculatorButton.Text = "BMR calculator";
+            this.bmrCalculatorButton.UseVisualStyleBackColor = true;
+            // 
+            // listOfCharitiesButton
+            // 
+            this.listOfCharitiesButton.Location = new System.Drawing.Point(411, 230);
+            this.listOfCharitiesButton.Name = "listOfCharitiesButton";
+            this.listOfCharitiesButton.Size = new System.Drawing.Size(183, 73);
+            this.listOfCharitiesButton.TabIndex = 24;
+            this.listOfCharitiesButton.Text = "List of charities";
+            this.listOfCharitiesButton.UseVisualStyleBackColor = true;
+            // 
+            // howLongIsAMarathonButton
+            // 
+            this.howLongIsAMarathonButton.Location = new System.Drawing.Point(411, 142);
+            this.howLongIsAMarathonButton.Name = "howLongIsAMarathonButton";
+            this.howLongIsAMarathonButton.Size = new System.Drawing.Size(183, 73);
+            this.howLongIsAMarathonButton.TabIndex = 23;
+            this.howLongIsAMarathonButton.Text = "How long is a marathon?";
+            this.howLongIsAMarathonButton.UseVisualStyleBackColor = true;
+            // 
+            // findOutMoreInformationLabel
+            // 
+            this.findOutMoreInformationLabel.AutoSize = true;
+            this.findOutMoreInformationLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.findOutMoreInformationLabel.Location = new System.Drawing.Point(234, 91);
+            this.findOutMoreInformationLabel.Name = "findOutMoreInformationLabel";
+            this.findOutMoreInformationLabel.Size = new System.Drawing.Size(287, 29);
+            this.findOutMoreInformationLabel.TabIndex = 26;
+            this.findOutMoreInformationLabel.Text = "Find out more information";
+            // 
+            // FindOutMoreForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.findOutMoreInformationLabel);
+            this.Controls.Add(this.bmrCalculatorButton);
+            this.Controls.Add(this.listOfCharitiesButton);
+            this.Controls.Add(this.howLongIsAMarathonButton);
+            this.Controls.Add(this.bmiCalculatorButton);
+            this.Controls.Add(this.previosRaceResultsButton);
+            this.Controls.Add(this.marathonSkills2015Button);
+            this.Name = "FindOutMoreForm";
+            this.Text = "Form1";
+            this.Controls.SetChildIndex(this.marathonSkills2015Button, 0);
+            this.Controls.SetChildIndex(this.previosRaceResultsButton, 0);
+            this.Controls.SetChildIndex(this.bmiCalculatorButton, 0);
+            this.Controls.SetChildIndex(this.panel1, 0);
+            this.Controls.SetChildIndex(this.footerPanel, 0);
+            this.Controls.SetChildIndex(this.howLongIsAMarathonButton, 0);
+            this.Controls.SetChildIndex(this.listOfCharitiesButton, 0);
+            this.Controls.SetChildIndex(this.bmrCalculatorButton, 0);
+            this.Controls.SetChildIndex(this.findOutMoreInformationLabel, 0);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.footerPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button marathonSkills2015Button;
+        private System.Windows.Forms.Button previosRaceResultsButton;
+        private System.Windows.Forms.Button bmiCalculatorButton;
+        private System.Windows.Forms.Button bmrCalculatorButton;
+        private System.Windows.Forms.Button listOfCharitiesButton;
+        private System.Windows.Forms.Button howLongIsAMarathonButton;
+        private System.Windows.Forms.Label findOutMoreInformationLabel;
+    }
+}
