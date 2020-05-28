7eb18f67fe76307e5604fc9f49104ebebb90252b
Айдар Сайфутдинов <stud0000192492@study.utmn.ru>
2020-05-28 23:15:22 +0500

    Форма "Add-Edit Charity"

---
+++ Marathons/Client/Add-EditCharity/Add-EditCharityForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Client.Add_EditCharity
+{
+    public partial class Add_EditCharityForm : BaseForm.BaseForm
+    {
+        public Add_EditCharityForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
