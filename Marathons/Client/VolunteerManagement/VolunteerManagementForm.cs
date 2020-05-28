7edc1cde929c91c7b2f57de4918e7596544215b1
Айдар Сайфутдинов <stud0000192492@study.utmn.ru>
2020-05-29 01:02:57 +0500

    Форма "Volunteer management"

---
+++ Marathons/Client/VolunteerManagement/VolunteerManagementForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Client.VolunteerManagement
+{
+    public partial class VolunteerManagementForm : BaseForm.BaseForm
+    {
+        public VolunteerManagementForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
